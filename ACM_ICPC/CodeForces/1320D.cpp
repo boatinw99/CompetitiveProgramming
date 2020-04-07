a490be261a8486c2bacb3840969dc05009a7613e
Thanatach Tangsakul <thanathatboat@hotmail.com>
2020-04-07 11:53:24 +0700

    resolve gitignore

--- ACM_ICPC/CodeForces/1320D.cpp
+++ ACM_ICPC/CodeForces/1320D.cpp
@@ -47,22 +47,22 @@ int main() {
         }
         updatehash(i);
     }
-    cin >> q ; 
-    for(int i=1,l,r,len ;i<=q;i++) {
-        bool good = 0 ;
-        cin >> l >> r >> len ; 
-        if(gethash(l,l+len-1)==gethash(r,r+len-1)) {
-            good++;
-        }
-        else if(lst[l+len-1]>=l&&lst[r+len-1]>=r) {
-            if(r%2==l%2) {
-                good+=(qs[l+len-1].se-qs[l-1].se==qs[r+len-1].se-qs[r-1].se&&qs[l+len-1].fi-qs[l-1].fi==qs[r+len-1].fi-qs[r-1].fi);
-            }
-            else {
-                good+=(qs[l+len-1].se-qs[l-1].se==qs[r+len-1].fi-qs[r-1].fi&&qs[l+len-1].fi-qs[l-1].fi==qs[r+len-1].se-qs[r-1].se);
-            } 
-        }
-        cout << (good?"Yes":"No") << '\n' ; 
-    }
-    return 0 ; 
+    // cin >> q ; 
+    // for(int i=1,l,r,len ;i<=q;i++) {
+    //     bool good = 0 ;
+    //     cin >> l >> r >> len ; 
+    //     if(gethash(l,l+len-1)==gethash(r,r+len-1)) {
+    //         good++;
+    //     }
+    //     else if(lst[l+len-1]>=l&&lst[r+len-1]>=r) {
+    //         if(r%2==l%2) {
+    //             good+=(qs[l+len-1].se-qs[l-1].se==qs[r+len-1].se-qs[r-1].se&&qs[l+len-1].fi-qs[l-1].fi==qs[r+len-1].fi-qs[r-1].fi);
+    //         }
+    //         else {
+    //             good+=(qs[l+len-1].se-qs[l-1].se==qs[r+len-1].fi-qs[r-1].fi&&qs[l+len-1].fi-qs[l-1].fi==qs[r+len-1].se-qs[r-1].se);
+    //         } 
+    //     }
+    //     cout << (good?"Yes":"No") << '\n' ; 
+    // }
+    // return 0 ; 
 }
