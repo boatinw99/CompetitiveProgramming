8bcf4f2bc201feae11e48141a8cc45bfec917d50
Thanatach Tangsakul <thanathatboat@hotmail.com>
2020-04-10 17:44:17 +0700

    set up code

--- CodeJam-prep.cpp
+++ CodeJam-prep.cpp
@@ -22,44 +22,31 @@ const ull MOD1 = 1e9+7 , MOD2 = 1e9+9 , MMOD1 = MOD1*MOD1 , MMOD2 = MOD2*MOD2;
 const ll INF = 1e18 ; 
 const double pi = 3.14159265358979 ; 
 const int N = 1e5+9 , inf = 1e9 , mod = 1e9+7 ; 
-
-
-// rolling hash
-puu hsh[N],pwhsh[N];
-ull PRIME1,PRIME2; 
-int str[N];
-void updatehash(int x) {
-    hsh[x] = {(hsh[x-1].fi*PRIME1+str[x])%MOD1,(hsh[x-1].se*PRIME2+str[x])%MOD2};
-}
-puu gethash(int l,int r) {
-    int len = r-l+1 ; 
-    return make_pair((hsh[r].fi - hsh[l-1].fi*pwhsh[len].fi+MMOD1)%MOD1,(hsh[r].se-hsh[l-1].se*pwhsh[len].se+MMOD2)%MOD2); 
-}
-ll gen_hashprime() {
-    PRIME1 = rand() + rand() ;
-    PRIME2 = rand() + rand() ;
-    pwhsh[0] = make_pair(1,1);
-    for(int i=1;i<N;i++) {
-        pwhsh[i] = make_pair((pwhsh[i-1].fi*PRIME1)%MOD1,(pwhsh[i-1].se*PRIME2)%MOD2);
-    }
-}
-///end rolling hash
-
-void add(ll &a,ll b) {
-    a+=b;
-    a%=mod ;
-}
 ll my_rand() {
     ll a = rand();
     ll b = rand();
     return a*(RAND_MAX+1)+b ; 
 }
+void add(ll &a,ll b) {
+    a+=b;
+    a%=mod ;
+}
 void init() {
-    // gen_hashprime();
+    ///clear the last test mem 
+}
+void solve() {
+    // start here 
+    init();
 }
 int main() {
     srand(chrono::steady_clock::now().time_since_epoch().count());
     ios::sync_with_stdio(false),cin.tie(0);
-    init();
+    int T ; 
+    cin >> T ;
+    for(int t=1;t<=T;t++) {
+        cout << "Case #" << t << ": " ;  
+        solve();
+        cout << '\n' ; 
+    }
     return 0 ;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+   vector<int> v ; 
+    v.pb(1);
+    v.eb(7);
+    v.eb(2);
+    v.eb(4);
+ 
